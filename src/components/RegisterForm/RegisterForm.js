321668116ba7b3b7424b270b41cd163fa8704129
moniet <moniet.sawhney@gmail.com>
2019-09-03 13:48:25 +0530

    added fade in for loader

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -13,7 +13,7 @@ const RegisterForm = ({ showError, setLoggedIn, loggedIn }) => {
 
     const handleSubmit = e => {
         e.preventDefault()
-        // document.querySelector('#loader').classList.remove('hide-loader');
+        document.querySelector('#loader').classList.remove('hide-loader');
         document.querySelector('#loader').classList.add('show-loader');
         API.registerUser(name, username, password)
             .then(data => {
@@ -24,6 +24,7 @@ const RegisterForm = ({ showError, setLoggedIn, loggedIn }) => {
 
                 if (data.errors) showError(data.errors)
 
+                document.querySelector('#loader').classList.add('hide-loader');
                 document.querySelector('#loader').classList.remove('show-loader');
             })
     }
